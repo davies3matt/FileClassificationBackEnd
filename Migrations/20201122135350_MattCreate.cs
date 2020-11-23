d48ecbe4151b39a3a2490f090b421c0c0b849712
Matt <53595224+davies3matt@users.noreply.github.com>
2020-11-23 12:27:23 +0200

    Add files via upload

---
+++ Migrations/20201122135350_MattCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace c_sharp_grad_backend.Migrations
+{
+    public partial class MattCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TableTextFiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AvatarOne = table.Column<byte[]>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: true),
+                    EthnicGroup = table.Column<string>(nullable: true),
+                    Gender = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    CellNumber = table.Column<string>(nullable: true),
+                    IDNumber = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TableTextFiles", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TableTextFiles");
+        }
+    }
+}
---
+++ Models/TextFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace c_sharp_grad_backend.Models
+{
+    public class TextFile
+    {
+        public int Id { get; set; }
+        public byte[] AvatarOne { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string EthnicGroup { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string CellNumber { get; set; }
+        public string IDNumber { get; set; }
+    }
+}
